ba04eba2f98e4c6c1599c017b1077f7419e7af69
StarkLin <liangyun.stark@gmail.com>
2025-02-28 16:32:17 +0800

    add input fields

---
+++ components/AppCard.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+
+type AppCardProps = { title: string; children: React.ReactNode };
+
+const AppCard: React.FC<AppCardProps> = ({ title, children }) => {
+  return (
+    <Card className="border-none shadow-none">
+      <CardHeader className="px-0">
+        <CardTitle>{title}</CardTitle>
+      </CardHeader>
+      <CardContent className="space-y-3 px-0">{children}</CardContent>
+    </Card>
+  );
+};
+
+export default AppCard;
--- components/inputs/TextField.tsx
+++ components/inputs/TextField.tsx
@@ -1,36 +1,42 @@
 "use client";
 
-import React, { InputHTMLAttributes } from 'react';
-import {useFormContext} from "react-hook-form";
-import {FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
-import {Input} from "@/components/ui/input";
-import {InputProps} from "@/components/inputs/types";
+import React, { InputHTMLAttributes } from "react";
+import { useFormContext } from "react-hook-form";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { InputProps } from "@/components/inputs/types";
 
 type Props = InputProps & InputHTMLAttributes<HTMLInputElement>;
 
 const TextField: React.FC<Props> = ({ label, name, description, ...props }) => {
-    const form = useFormContext();
+  const form = useFormContext();
 
-    return (
-        <FormField
-            control={form.control}
-            name={name}
-            render={({field}) => (
-                <FormItem>
-                    {label && <FormLabel>{label}</FormLabel>}
-                    <FormControl>
-                        <Input {...props} {...field} />
-                    </FormControl>
-                    {description && (
-                        <FormDescription>
-                            {description}
-                        </FormDescription>
-                    )}
-                    <FormMessage/>
-                </FormItem>
-            )}
-        />
-    );
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex flex-col gap-1">
+          {label && <FormLabel>{label}</FormLabel>}
+
+          <FormControl>
+            <Input {...props} {...field} />
+          </FormControl>
+
+          {description && <FormDescription>{description}</FormDescription>}
+
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
 };
 
 export default TextField;
